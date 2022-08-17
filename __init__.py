b5dc5db8580b98e7934ff710c41615df9e8d30a0
betty <huynh.dh2310@gmail.com>
2022-08-17 12:24:37 +0700

    feat: init button in add card editor

--- __init__.py
+++ __init__.py
@@ -1,6 +1,15 @@
+import json
+import re
+import os
+
 from aqt import mw
 from aqt.utils import showInfo, qconnect
-import aqt.qt as qt
+from aqt.qt import *
+
+from anki.buildinfo import version
+from anki.hooks import addHook
+
+from . import utils
 
 
 def testFunction() -> None:
@@ -8,8 +17,29 @@ def testFunction() -> None:
     showInfo("💳 Card count: %d" % cardCount)
 
 
-action = qt.QAction("Jisho auto-add", mw)
-qt.qconnect(action.triggered, testFunction)
-menu = qt.QMenu('&Jisho auto-add', mw)
-action = menu.addAction("&Count all cards")
-mw.form.menuTools.addMenu(menu)
+def setupGuiMenu() -> None:
+    action = QAction("Jisho auto-add", mw)
+    mw.form.menuTools.addSeparator()
+    mw.form.menuTools.addAction(action)
+
+
+def addButtons(buttons, editor):
+    editor._links["generateFurigana"] = lambda ed=editor: doIt(
+        ed, generate)
+    return buttons + [
+        editor._addButton(os.path.join(os.path.dirname(__file__), "icons",
+                          "japanese.png"), "generate", tip=u"automatic generate form Jisho")
+    ]
+
+
+def doIt(editor, action):
+    utils.Selection(editor, lambda s: action(editor, s))
+
+
+def generate(editor, s):
+    return
+
+
+setupGuiMenu()
+addHook("setupEditorButtons", addButtons)
+# qconnect(action.triggered, testFunction)
